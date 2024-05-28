227eb5b40d6f2d8110244ea521d4e9df73f7bddf
Lucas dos Santos Gomes <lucas.gomes@telefonica.com>
2024-05-28 11:49:20 -0300

    Requisição da starlink feita

--- src/components/StarlinkList.jsx
+++ src/components/StarlinkList.jsx
@@ -1,28 +1,25 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function StarlinkList() {
-  //const [starlinks, setStarlinks] = useState([]);
-  
-  const fetchStarlink = async () => {
-    const request = {
-      query: {},
-      options: { limit: 100 }
-    };
+  const [starlinks, setStarlinks] = useState([]);
 
-    const teste = await fetch('https://api.spacexdata.com/v4/starlink/query', {
-      method: 'POST',
-      body: JSON.stringify(request)
-    });
-
-    console.log(teste);
-  }
   useEffect(() => {
-    fetchStarlink();
+    axios.post('https://api.spacexdata.com/v4/starlink/query', {
+      "query": {},
+      "options": { limit: 100 }
+    }).then(res => setStarlinks(res.data.docs)).catch(err => {
+      alert('Houve um erro na requisição');
+      console.log(err);
+    });
   }, []);
 
   return (
     <>
-      <h1>Olá.. </h1>
+      <h1>Satélites da Starlink</h1>
+      {starlinks.length === 0 ? (<p>Carregando satélites...</p>) : starlinks.map(satellite => (
+        <li key={satellite.id}>{satellite.spaceTrack.OBJECT_NAME}</li>
+      ))}
     </>
   );
 }
